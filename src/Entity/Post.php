6304fd1d845071a9b583b8b11a6e4faa2d0a51d6
Ludovic V <vinga.ludovic@gmail.com>
2025-05-06 16:50:26 +0200

    feat: gestion des articles, des images et miniatures en cours

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,7 +5,14 @@ namespace App\Entity;
 use App\Repository\PostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
 
+
+
+#[Vich\Uploadable]
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 class Post
 {
@@ -17,22 +24,50 @@ class Post
     #[ORM\ManyToOne(inversedBy: 'posts')]
     private ?User $user = null;
 
+
+    #[Assert\NotBlank(message: 'La catégorie est obligatoire.')]
+    #[Assert\Type(
+        type: Category::class,
+        message: 'La catégorie n\'existe pas.',
+    )]
     #[ORM\ManyToOne(inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
 
+
+    #[Assert\NotBlank(message: 'Le titre est obligatoire.')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le titre ne doit pas dépasser {{ limit }} caractères.',
+    )]
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
+
+    #[Gedmo\Slug(fields: ['title'])]
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
+
 
     #[ORM\Column]
     private ?bool $isPublished = null;
 
+
+    // NOTE: This is not a mapped field of entity metadata, just a simple property.
+    #[Assert\File(
+        maxSize: '3M',
+        extensions: ['png', 'jpg', 'jpeg', 'webp'],
+        extensionsMessage: 'Seul les formats png, jpg, jpeg et webp sont valides. ',
+    )]
+    #[Vich\UploadableField(mapping: 'posts', fileNameProperty: 'image')]
+    private ?File $imageFile = null;
+
+
     #[ORM\Column(length: 255, nullable: true, unique: true)]
     private ?string $image = null;
 
+
+    #[Assert\NotBlank(message: 'Le contenu est obligatoire')]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
 
@@ -44,6 +79,23 @@ class Post
 
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $publishedAt = null;
+
+
+    #[Assert\NotBlank(message: 'La description est obligatoire')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'La description ne doit pas dépasser {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Les mots clés ne doivent pas dépasser {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $keywords = null;
 
     public function getId(): ?int
     {
@@ -110,6 +162,33 @@ class Post
         return $this;
     }
 
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) 
+        {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
     public function getImage(): ?string
     {
         return $this->image;
@@ -166,6 +245,30 @@ class Post
     public function setPublishedAt(?\DateTimeImmutable $publishedAt): static
     {
         $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getKeywords(): ?string
+    {
+        return $this->keywords;
+    }
+
+    public function setKeywords(?string $keywords): static
+    {
+        $this->keywords = $keywords;
 
         return $this;
     }
